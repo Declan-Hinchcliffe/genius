33f380449bc4f1819af0785bfc49191d0807c34b
Joe Bricknell <joe.bricknell@skybettingandgaming.com>
2020-09-24 12:01:56 +0100

    [refs master] can retrieve all songs by artist using artist search

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ type Song struct {
 	Artist string
 }
 
-// need to define our song struct
+//need to define our song struct
 type data struct {
 	Response struct {
 		Hits []hit
@@ -64,6 +64,49 @@ type hit struct {
 	} `json:"result"`
 }
 
+type allSongs struct {
+	Meta struct {
+		Status int `json:"status"`
+	} `json:"meta"`
+	Response struct {
+		Songs []struct {
+			AnnotationCount          int    `json:"annotation_count"`
+			APIPath                  string `json:"api_path"`
+			FullTitle                string `json:"full_title"`
+			HeaderImageThumbnailURL  string `json:"header_image_thumbnail_url"`
+			HeaderImageURL           string `json:"header_image_url"`
+			ID                       int    `json:"id"`
+			LyricsOwnerID            int    `json:"lyrics_owner_id"`
+			LyricsState              string `json:"lyrics_state"`
+			Path                     string `json:"path"`
+			PyongsCount              int    `json:"pyongs_count"`
+			SongArtImageThumbnailURL string `json:"song_art_image_thumbnail_url"`
+			SongArtImageURL          string `json:"song_art_image_url"`
+			Stats                    struct {
+				UnreviewedAnnotations int  `json:"unreviewed_annotations"`
+				Concurrents           int  `json:"concurrents"`
+				Hot                   bool `json:"hot"`
+				Pageviews             int  `json:"pageviews"`
+			} `json:"stats,omitempty"`
+			Title             string `json:"title"`
+			TitleWithFeatured string `json:"title_with_featured"`
+			URL               string `json:"url"`
+			PrimaryArtist     struct {
+				APIPath        string `json:"api_path"`
+				HeaderImageURL string `json:"header_image_url"`
+				ID             int    `json:"id"`
+				ImageURL       string `json:"image_url"`
+				IsMemeVerified bool   `json:"is_meme_verified"`
+				IsVerified     bool   `json:"is_verified"`
+				Name           string `json:"name"`
+				URL            string `json:"url"`
+				Iq             int    `json:"iq"`
+			} `json:"primary_artist,omitempty"`
+		} `json:"songs"`
+		NextPage int `json:"next_page"`
+	} `json:"response"`
+}
+
 // Lyrics represents the lyrics returned from the lyric api
 type Lyrics struct {
 	Lyrics string `json:"lyrics"`
@@ -78,10 +121,16 @@ func main() {
 	flag.StringVar(&wordFlag, "word", "", "specify the words you want to look for")
 	flag.Parse()
 
-	// to search for all songs by artist need to first search by name
-	// pluck out the arist id from the json response (under primary artist)
 	// insert this into the api.genius.com/artists/:id/songs api
 	// this will return all songs by an artist
+
+	artistFlag = `drake`
+	songs, err := getSongsByArtist(artistFlag)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(songs)
 
 	lyrics, err := getTheLyrics(searchFlag)
 	if err != nil {
@@ -107,6 +156,104 @@ func main() {
 		keys[1], wordMap[keys[1]],
 		keys[2], wordMap[keys[2]],
 		keys[3], wordMap[keys[3]])
+}
+
+// allSongsByArtist will return all the songs by a given artist
+func getSongsByArtist(artistFlag string) ([]Song, error) {
+	id, err := getArtistID(artistFlag)
+	if err != nil {
+		return nil, err
+	}
+
+	songs, err := songsByArtist(*id)
+
+	return songs, nil
+}
+
+// getArtistID will call to the genius api search and pull out the artist id from the first search result
+func getArtistID(artistFlag string) (*int, error) {
+	encodedSearch := url.QueryEscape(artistFlag)
+	// build request to genius api
+	req, err := http.NewRequest("GET", fmt.Sprintf("https://api.genius.com/search?q=%v", encodedSearch), strings.NewReader(""))
+	if err != nil {
+		return nil, err
+	}
+
+	token := "SWIZahaJ5gY3S8ZOAwLbTlpREdKOXMakvPPM_0vD5q1AXId4J4fGTDJ-VO-h0Ojp"
+	req.Header.Set("Authorization", "Bearer "+token)
+	req.Header.Set("Content-Type", "Application/json")
+
+	// make request to genius api
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	// read the body of the request
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// unmarshal json into song response struct
+	var apiSongResponse data
+	if err := json.Unmarshal(body, &apiSongResponse); err != nil {
+		return nil, err
+	}
+
+	id := apiSongResponse.Response.Hits[0].Result.PrimaryArtist.ID
+
+	return &id, nil
+}
+
+func songsByArtist(id int) ([]Song, error) {
+	// build request using the id obtained earlier
+	req, err := http.NewRequest("GET", fmt.Sprintf("https://api.genius.com/artists/%v/songs?sort=popularity", id), strings.NewReader(""))
+	if err != nil {
+		return nil, err
+	}
+
+	token := "SWIZahaJ5gY3S8ZOAwLbTlpREdKOXMakvPPM_0vD5q1AXId4J4fGTDJ-VO-h0Ojp"
+	req.Header.Set("Authorization", "Bearer "+token)
+	req.Header.Set("Content-Type", "Application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var apiSongResponse allSongs
+	if err := json.Unmarshal(body, &apiSongResponse); err != nil {
+		return nil, err
+	}
+
+	songs, err := getSongs(apiSongResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return songs, nil
+}
+
+func getSongs(apiResponse allSongs) ([]Song, error) {
+	// define our song list variable and range over the songs and add the
+	// song name and artist to the song struct
+	var songList []Song
+	for _, songs := range apiResponse.Response.Songs {
+		song := Song{
+			Title:  strings.TrimSpace(songs.Title),
+			Artist: strings.TrimSpace(songs.PrimaryArtist.Name),
+		}
+
+		songList = append(songList, song)
+	}
+
+	return songList, nil
 }
 
 // getTheLyrics will call to the genius api to get the songs and then call
@@ -144,7 +291,7 @@ func searchSongs(search string) ([]Song, error) {
 	// build request to genius api
 	req, err := http.NewRequest("GET", fmt.Sprintf("https://api.genius.com/search?q=%v", search), strings.NewReader(""))
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	token := "SWIZahaJ5gY3S8ZOAwLbTlpREdKOXMakvPPM_0vD5q1AXId4J4fGTDJ-VO-h0Ojp"
@@ -154,19 +301,19 @@ func searchSongs(search string) ([]Song, error) {
 	// make request to genius api
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	// read the body of the request
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	// unmarshal json into song response struct
 	var apiSongResponse data
 	if err := json.Unmarshal(body, &apiSongResponse); err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	// define our song list variable and range over the songs and add the
