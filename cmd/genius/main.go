6ccd7bfa9b226046e07115a684591967a461f7f5
Joe Bricknell <joe.bricknell@skybettingandgaming.com>
2020-09-25 09:38:55 +0100

    [refs master] added some comments

--- cmd/genius/main.go
+++ cmd/genius/main.go
@@ -4,6 +4,15 @@ import (
 	"github.com/joe-bricknell/genius"
 )
 
+// This project will allow you to search for a particular artist or term
+// and will return you the number of times a given word is used within
+// the lyrics search result
 func main() {
 	genius.Genius()
 }
+
+// todo: need to add unit tests around functions
+// todo: need to add go routines for concurrent requests
+// todo: possibly try and benchmark app to see before and after go routines?
+// todo: need to add flag for search words
+// todo: add different searches, for a specific song or something
