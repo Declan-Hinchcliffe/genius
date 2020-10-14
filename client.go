79126ea834d47150470edf5c9c0b5cfb60360236
Joe Bricknell <joe.bricknell@skybettingandgaming.com>
2020-10-14 08:19:24 +0100

    [refs 3-wip-refactor] minor refactoring

--- client.go
+++ client.go
@@ -3,9 +3,12 @@ package genius
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
-var client = &http.Client{}
+var client = &http.Client{
+	Timeout: time.Second * 5,
+}
 
 func makeRequest(c CustomClient, endpoint string) (*http.Response, error) {
 	req, err := http.NewRequest("GET", fmt.Sprintf("%v/%v", c.url, endpoint), nil)
