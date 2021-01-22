7f90c9df4569835c5a96db23a7dcba28cb937116
Joe Bricknell <joe.bricknell@skybettingandgaming.com>
2021-01-22 15:33:06 +0000

    [refs 10-feat-post] some react stuff

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Axios from 'axios'
 
 function Home() {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+     
+    }, [count])
+
+    Axios.post('http://localhost:9000/song/lyrics', {
+        name: "wap"
+    })
+    .then((response) => {
+        console.log(response);
+    })
+
+
     return (
         <div>
-            
+            <form>
+                <label>Search: </label>
+                <input className="border rounded-lg"/>
+                <button type="submit" className="w-20 border rounded-lg" onClick={() => setCount(count + 1)}>Submit</button>
+            </form>
         </div>
     )
 }
